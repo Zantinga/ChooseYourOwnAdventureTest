9cca0f690f09b5c6bfe5161363f240afaaaf8e2a
Jan Zantinga <janzantinga@gmail.com>
2016-11-03 23:01:12 -0400

    implementing new architecutre
    created storyPoints and started using them

--- app/src/main/java/janzantinga/com/chooseyourownadventuretest/MainActivity.java
+++ app/src/main/java/janzantinga/com/chooseyourownadventuretest/MainActivity.java
@@ -3,25 +3,98 @@ package janzantinga.com.chooseyourownadventuretest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static GridView storySoFar;
     public static Button button1, button2;
     public static int storyPath;
-    public static int button1Value, button2Value;
-    public static String button1Info, button2Info;
     public static List<String> storyArray;
     public static ArrayAdapter<String> gridViewArrayAdapter;
+    public static StoryPoint storyPoint;
+
+    //TODO: ADD TYPEFACES
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialize();
+
+        storyArray.add("hello");
+        gridViewArrayAdapter.notifyDataSetChanged();
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button1.setVisibility(View.INVISIBLE);
+                button2.setVisibility(View.INVISIBLE);
+                storyPoint = ProgressStory.getNextStoryPoint(storyPoint.getButton1Value());
+
+                gridViewArrayAdapter.notifyDataSetChanged();
+
+                storySoFar.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Select the last row so it will scroll into view...
+                        storySoFar.setSelection(gridViewArrayAdapter.getCount() - 1);
+                    }
+                });
+                button1.setText(storyPoint.getButton1Value());
+                button2.setText(storyPoint.getButton2Value());
+                button1.setVisibility(View.VISIBLE);
+                button2.setVisibility(View.VISIBLE);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button1.setVisibility(View.INVISIBLE);
+                button2.setVisibility(View.INVISIBLE);
+                storyPoint = ProgressStory.getNextStoryPoint(storyPoint.getButton2Value());
+
+                gridViewArrayAdapter.notifyDataSetChanged();
+
+                storySoFar.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Select the last row so it will scroll into view...
+                        storySoFar.setSelection(gridViewArrayAdapter.getCount() - 1);
+                    }
+                });
+                button1.setText(storyPoint.getButton1Value());
+                button2.setText(storyPoint.getButton2Value());
+                button1.setVisibility(View.VISIBLE);
+                button2.setVisibility(View.VISIBLE);utton2.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    public void initialize() {
+        storyArray = new ArrayList<String>();
+        storyPoint = new StoryPoint(new ArrayList<String>(), "optionA", "optionB", 1, 2);
+
+        gridViewArrayAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, storyArray
+        );
+
+        storySoFar = (GridView) findViewById(R.id.gridView1);
+        storySoFar.setAdapter(gridViewArrayAdapter);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d("Debug", "onStart has been called");
     }
 
     @Override
--- app/src/main/java/janzantinga/com/chooseyourownadventuretest/ProgressStory.java
+++ app/src/main/java/janzantinga/com/chooseyourownadventuretest/ProgressStory.java
@@ -1,12 +1,22 @@
 package janzantinga.com.chooseyourownadventuretest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Jan Zantinga on 11/3/2016.
  */
 
 public class ProgressStory {
 
-    public StoryPoint ProgressStory(int nextStoryPoint) {
+    public static StoryPoint getNextStoryPoint(int nextStoryPoint) {
+        List<String> storyPoints = new ArrayList<String>();
+        switch(nextStoryPoint) {
+            case 1:
+                break;
+            default:
+                return new StoryPoint();
+        }
         return new StoryPoint();
     }
 }
